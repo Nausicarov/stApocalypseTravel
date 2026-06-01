e8c0f2d9c6e674ead9e3a97c9b0dcc0db6104c4c
eunomia nausicarov <17081220+eunomia-nausicarov@user.noreply.gitee.com>
2026-06-01 09:44:23 +0800

    feat: 初始化状态栏界面与变量管理系统
    新增：
    - 第一条测试消息文件
    - 状态栏前端页面、样式与Vue组件
    - 变量管理相关的规则、列表、初始化与输出格式文件
    - MVU变量更新脚本与schema类型定义
    - 全局状态存储与挂载逻辑
    
    更新：补全了角色卡的世界书配置与脚本库引用

---
+++ dist/stTraveller/脚本/变量结构/index.js
@@ -0,0 +1,78 @@
+import * as __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_StageDog_tavern_resource_dist_util_mvu_zod_js_8998c919__ from "https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/dist/util/mvu_zod.js";
+
+var __webpack_modules__ = {
+  "./src/stTraveller/schema.ts"(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+    eval('{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Schema: () => (/* binding */ Schema)\n/* harmony export */ });\n/* harmony import */ var zod__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! zod */ "zod");\n/* harmony import */ var zod__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(zod__WEBPACK_IMPORTED_MODULE_0__);\n\nconst Schema = zod__WEBPACK_IMPORTED_MODULE_0__.z.object({});\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3RUcmF2ZWxsZXIvc2NoZW1hLnRzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBTyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQyxrQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbInNyYzovL3RhdmVybl9oZWxwZXJfdGVtcGxhdGUvc3JjL3N0VHJhdmVsbGVyL3NjaGVtYS50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgU2NoZW1hID0gei5vYmplY3Qoe30pO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/stTraveller/schema.ts\n\n}');
+  },
+  "./src/stTraveller/脚本/变量结构/index.ts"(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+    eval('{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var https_testingcf_jsdelivr_net_gh_StageDog_tavern_resource_dist_util_mvu_zod_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/dist/util/mvu_zod.js */ "https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/dist/util/mvu_zod.js");\n/* harmony import */ var _schema__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../schema */ "./src/stTraveller/schema.ts");\n\n\n$(() => {\n    (0,https_testingcf_jsdelivr_net_gh_StageDog_tavern_resource_dist_util_mvu_zod_js__WEBPACK_IMPORTED_MODULE_0__.registerMvuSchema)(_schema__WEBPACK_IMPORTED_MODULE_1__.Schema);\n});\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3RUcmF2ZWxsZXIv6ISa5pysL+WPmOmHj+e7k+aehC9pbmRleC50cyIsIm1hcHBpbmdzIjoiOzs7QUFBb0g7QUFDOUU7QUFFdEMsQ0FBQyxDQUFDLEdBQUcsRUFBRTtJQUNMLGdJQUFpQixDQUFDLDJDQUFNLENBQUMsQ0FBQztBQUM1QixDQUFDLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsic3JjOi8vdGF2ZXJuX2hlbHBlcl90ZW1wbGF0ZS9zcmMvc3RUcmF2ZWxsZXIv6ISa5pysL+WPmOmHj+e7k+aehC9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyByZWdpc3Rlck12dVNjaGVtYSB9IGZyb20gJ2h0dHBzOi8vdGVzdGluZ2NmLmpzZGVsaXZyLm5ldC9naC9TdGFnZURvZy90YXZlcm5fcmVzb3VyY2UvZGlzdC91dGlsL212dV96b2QuanMnO1xuaW1wb3J0IHsgU2NoZW1hIH0gZnJvbSAnLi4vLi4vc2NoZW1hJztcblxuJCgoKSA9PiB7XG4gIHJlZ2lzdGVyTXZ1U2NoZW1hKFNjaGVtYSk7XG59KTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/stTraveller/脚本/变量结构/index.ts\n\n}');
+  },
+  "https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/dist/util/mvu_zod.js"(module) {
+    module.exports = __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_StageDog_tavern_resource_dist_util_mvu_zod_js_8998c919__;
+  },
+  zod(module) {
+    module.exports = z;
+  }
+};
+
+var __webpack_module_cache__ = {};
+
+function __webpack_require__(moduleId) {
+  var cachedModule = __webpack_module_cache__[moduleId];
+  if (cachedModule !== undefined) {
+    return cachedModule.exports;
+  }
+  var module = __webpack_module_cache__[moduleId] = {
+    exports: {}
+  };
+  if (!(moduleId in __webpack_modules__)) {
+    delete __webpack_module_cache__[moduleId];
+    var e = new Error("Cannot find module '" + moduleId + "'");
+    e.code = "MODULE_NOT_FOUND";
+    throw e;
+  }
+  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  return module.exports;
+}
+
+(() => {
+  __webpack_require__.n = module => {
+    var getter = module && module.__esModule ? () => module["default"] : () => module;
+    __webpack_require__.d(getter, {
+      a: getter
+    });
+    return getter;
+  };
+})();
+
+(() => {
+  __webpack_require__.d = (exports, definition) => {
+    for (var key in definition) {
+      if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+        Object.defineProperty(exports, key, {
+          enumerable: true,
+          get: definition[key]
+        });
+      }
+    }
+  };
+})();
+
+(() => {
+  __webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
+})();
+
+(() => {
+  __webpack_require__.r = exports => {
+    if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+      Object.defineProperty(exports, Symbol.toStringTag, {
+        value: "Module"
+      });
+    }
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+  };
+})();
+
+var __webpack_exports__ = __webpack_require__("./src/stTraveller/脚本/变量结构/index.ts");
